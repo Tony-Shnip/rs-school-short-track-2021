c1e0985751ede43ff095566a75632a14c03c231c
Tony-Shnip <shnip12345@gmail.com>
2021-04-09 22:45:42 +0300

    add: add 7 task solution

--- src/07-file-names.js
+++ src/07-file-names.js
@@ -13,8 +13,31 @@
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new Error('Not implemented');
+function renameFiles(names) {
+  const ans = names;
+  let test = [];
+  let k = 0;
+  let cic = true;
+  while (cic) {
+    cic = false;
+    for (let i = 0; i < names.length; i++) {
+      if (names.indexOf(ans[i], i + 1) !== -1) {
+        test.push(ans[i]);
+        cic = true;
+      }
+    }
+    for (let i = 0; i < test.length; i++) {
+      k = 0;
+      for (let j = 0; j < ans.length; j++) {
+        if (ans[j] === test[i]) {
+          if (k !== 0) ans[j] = ans[j].concat('(', String(k), ')');
+          k++;
+        }
+      }
+    }
+    test = [];
+  }
+  return ans;
 }
 
 module.exports = renameFiles;
