9b4a67d01d95315438c1e97a5bee37cc8ac601c9
Tony-Shnip <shnip12345@gmail.com>
2021-04-10 23:44:05 +0300

    add: add 11 task solution

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,22 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const set = new Set();
+  const ans = [];
+  let k = 0;
+  for (let i = 0; i < str.length; i++) {
+    set.add(str[i]);
+  }
+  set.forEach((elem) => {
+    for (let i = 0; i < str.length; i++) {
+      if (elem === str[i]) k++;
+    }
+    if (k !== 1) ans.push(String(k).concat(elem));
+    else ans.push(elem);
+    k = 0;
+  });
+  return ans.join('');
 }
 
 module.exports = encodeLine;
